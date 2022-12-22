508c02f36eec8588f8ef0130784c05e81e69b1bd
CarmenCruces <karmelacfer@gmail.com>
2022-12-22 23:24:06 +0100

    final commit

--- js/index.js
+++ js/index.js
@@ -12,18 +12,18 @@ fetchDiscsJson().then(discs =>{
         const discsSection = document. getElementById('discSection');
 
         let id = discs.discografia[index].id;
-        let caratula = discs.discografia[index].caratula;
+        let portada = discs.discografia[index].portada;
         let titulo = discs.discografia[index].titulo;
         let discografica = discs.discografia[index].discografica;
         let fecha = discs.discografia[index].fecha;
 
-        discSection.innerHTML += `
+        discsSection.innerHTML += `
         <div class="card" style="width: 18rem;">
-            <img src="${caratula}" class="card-img-top" alt="caratula del disco">
+            <img src="${portada}" class="card-img-top" alt="portada del disco">
             <div class="card-body">
                 <h2 class="card-title">${id}. ${titulo}</h2>
-                <h4 class="card-title">Discografica: ${discografica}</h4>
-                <h4 class="card-title">Fecha: ${fecha}</h4>
+                <h3 class="card-title">Discografica: ${discografica}</h3>
+                <h3 class="card-title">Fecha: ${fecha}</h3>
             </div>
         </div>
         ` 
